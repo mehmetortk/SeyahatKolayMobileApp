53fa2b35fb1aa94c0dde780197998a5f8405de9d
mehmeterenortak <mehmet_310gs@hotmail.com>
2024-03-16 19:20:29 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
+
+}
+
+android {
+    namespace = "com.example.firebasemobileapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.firebasemobileapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildToolsVersion = "34.0.0"
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-analytics-ktx:20.0.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.0.0")
+
+
+}
---
+++ app/src/main/java/com/example/firebasemobileapp/Login.java
@@ -0,0 +1,71 @@
+package com.example.firebasemobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Login extends AppCompatActivity {
+    FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        EditText txtUsername, txtPassword;
+        Button btnLogin;
+
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                firestore.collection("users").document("user1")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        String actualPassword = document.getString("password");
+                                        if (actualPassword != null && actualPassword.equals(password)) {
+                                            Toast.makeText(Login.this, "Success", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(Login.this, MainPage.class);
+                                            startActivity(intent);
+                                        } else {
+                                            Toast.makeText(Login.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                                        }
+                                    } else {
+                                        Toast.makeText(Login.this, "User not found!!", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Exception exception = task.getException();
+                                    if (exception != null) {
+                                        Log.e("FirestoreError", "Error retrieving user data: " + exception.getMessage());
+                                    }
+                                    Toast.makeText(Login.this, "Error retrieving data. Please try again later", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
